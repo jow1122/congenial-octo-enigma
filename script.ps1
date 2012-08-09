52fbd2a11b0db76becf8ebb5c28bc0eda9bebf4f
jow1122 <ridol71534@evnft.com>
2012-08-09 19:56:00 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -5,3 +5,4 @@ Temporary commit content for 2012-07-23T19:56:00
 Temporary commit content for 2012-07-28T19:56:00
 Temporary commit content for 2012-08-03T19:56:00
 Temporary commit content for 2012-08-07T19:56:00
+Temporary commit content for 2012-08-09T19:56:00
