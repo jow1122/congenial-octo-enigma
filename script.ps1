63e46f4fd49e307c0ae509d624c4e341f1ac692b
jow1122 <ridol71534@evnft.com>
2013-01-23 19:56:00 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -18,3 +18,4 @@ Temporary commit content for 2012-12-04T19:56:00
 Temporary commit content for 2012-12-06T19:56:00
 Temporary commit content for 2012-12-16T19:56:00
 Temporary commit content for 2013-01-18T19:56:00
+Temporary commit content for 2013-01-23T19:56:00
