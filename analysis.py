8386396b2d7cdbc5e18fc44eb438c441341c1c47
jow1122 <ridol71534@evnft.com>
2013-02-01 19:56:00 +0100

    Added validation for user input

--- analysis.py
+++ analysis.py
@@ -19,3 +19,4 @@ Temporary commit content for 2012-12-12T19:56:00
 Temporary commit content for 2013-01-03T19:56:00
 Temporary commit content for 2013-01-07T19:56:00
 Temporary commit content for 2013-01-31T19:56:00
+Temporary commit content for 2013-02-01T19:56:00
