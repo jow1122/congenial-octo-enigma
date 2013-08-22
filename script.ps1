2245f0513c9c281d4141400a579f3d7aa13d83eb
jow1122 <ridol71534@evnft.com>
2013-08-22 19:56:00 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -42,3 +42,4 @@ Temporary commit content for 2013-07-29T19:56:00
 Temporary commit content for 2013-08-08T19:56:00
 Temporary commit content for 2013-08-14T19:56:00
 Temporary commit content for 2013-08-18T19:56:00
+Temporary commit content for 2013-08-22T19:56:00
