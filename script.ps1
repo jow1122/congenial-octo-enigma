52b57c8b64d45f51221f6bf36b0411a5b3ea2d3b
jow1122 <ridol71534@evnft.com>
2013-09-13 19:56:00 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -44,3 +44,4 @@ Temporary commit content for 2013-08-14T19:56:00
 Temporary commit content for 2013-08-18T19:56:00
 Temporary commit content for 2013-08-22T19:56:00
 Temporary commit content for 2013-09-11T19:56:00
+Temporary commit content for 2013-09-13T19:56:00
