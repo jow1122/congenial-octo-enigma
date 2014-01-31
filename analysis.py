b8fe13f2aab060a9e7c6351df16fd0c86fcf8830
jow1122 <ridol71534@evnft.com>
2014-01-31 19:56:00 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -43,3 +43,4 @@ Temporary commit content for 2014-01-02T19:56:00
 Temporary commit content for 2014-01-05T19:56:00
 Temporary commit content for 2014-01-13T19:56:00
 Temporary commit content for 2014-01-16T19:56:00
+Temporary commit content for 2014-01-31T19:56:00
