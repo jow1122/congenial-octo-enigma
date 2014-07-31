29e15072db4d3b7ecb167299496faf4f194260de
jow1122 <ridol71534@evnft.com>
2014-07-31 19:56:00 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -69,3 +69,4 @@ Temporary commit content for 2014-06-24T19:56:00
 Temporary commit content for 2014-06-27T19:56:00
 Temporary commit content for 2014-07-09T19:56:00
 Temporary commit content for 2014-07-15T19:56:00
+Temporary commit content for 2014-07-31T19:56:00
