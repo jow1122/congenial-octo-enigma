1e469e45870161953fe31657348307ba41a2b506
jow1122 <ridol71534@evnft.com>
2015-04-12 19:56:00 +0100

    Optimized database queries

--- script.ps1
+++ script.ps1
@@ -92,3 +92,4 @@ Temporary commit content for 2015-03-16T19:56:00
 Temporary commit content for 2015-03-21T19:56:00
 Temporary commit content for 2015-04-01T19:56:00
 Temporary commit content for 2015-04-05T19:56:00
+Temporary commit content for 2015-04-12T19:56:00
