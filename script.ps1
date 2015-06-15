6427e670f7260f3d3c32c40baa1e6386416274aa
jow1122 <ridol71534@evnft.com>
2015-06-15 19:56:00 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -98,3 +98,4 @@ Temporary commit content for 2015-04-19T19:56:00
 Temporary commit content for 2015-04-24T19:56:00
 Temporary commit content for 2015-04-25T19:56:00
 Temporary commit content for 2015-05-22T19:56:00
+Temporary commit content for 2015-06-15T19:56:00
