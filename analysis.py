53551d7aafea36e47a8a6478e585d3ed7117c8b2
jow1122 <ridol71534@evnft.com>
2015-08-13 19:56:00 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -99,3 +99,4 @@ Temporary commit content for 2015-06-27T19:56:00
 Temporary commit content for 2015-07-06T19:56:00
 Temporary commit content for 2015-07-08T19:56:00
 Temporary commit content for 2015-07-30T19:56:00
+Temporary commit content for 2015-08-13T19:56:00
