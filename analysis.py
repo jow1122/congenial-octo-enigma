9d919132067546b48ace88c5bf0c9db35cee759c
jow1122 <ridol71534@evnft.com>
2016-01-09 19:56:00 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -117,3 +117,4 @@ Temporary commit content for 2015-11-26T19:56:00
 Temporary commit content for 2015-12-25T19:56:00
 Temporary commit content for 2016-01-06T19:56:00
 Temporary commit content for 2016-01-08T19:56:00
+Temporary commit content for 2016-01-09T19:56:00
