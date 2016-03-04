7b9028908fe6f663eb41d47bf41768c6f5f79b7a
jow1122 <ridol71534@evnft.com>
2016-03-04 19:56:00 +0100

    Fixed bug in data processing script

--- script.ps1
+++ script.ps1
@@ -116,3 +116,4 @@ Temporary commit content for 2015-12-06T19:56:00
 Temporary commit content for 2016-01-10T19:56:00
 Temporary commit content for 2016-01-18T19:56:00
 Temporary commit content for 2016-01-24T19:56:00
+Temporary commit content for 2016-03-04T19:56:00
