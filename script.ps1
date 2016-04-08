e3b038156d152d92032ad88810c50371cf48643e
jow1122 <ridol71534@evnft.com>
2016-04-08 19:56:00 +0100

    Enhanced UI responsiveness

--- script.ps1
+++ script.ps1
@@ -120,3 +120,4 @@ Temporary commit content for 2016-03-04T19:56:00
 Temporary commit content for 2016-03-18T19:56:00
 Temporary commit content for 2016-03-21T19:56:00
 Temporary commit content for 2016-04-04T19:56:00
+Temporary commit content for 2016-04-08T19:56:00
