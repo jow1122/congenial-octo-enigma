06e7b356d4721f16c7508244bfb5dea582a53ce1
jow1122 <ridol71534@evnft.com>
2016-08-11 19:56:00 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -131,3 +131,4 @@ Temporary commit content for 2016-07-19T19:56:00
 Temporary commit content for 2016-07-22T19:56:00
 Temporary commit content for 2016-08-08T19:56:00
 Temporary commit content for 2016-08-10T19:56:00
+Temporary commit content for 2016-08-11T19:56:00
