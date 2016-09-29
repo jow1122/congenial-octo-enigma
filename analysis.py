389f8ef4efa4381e4c064ae793eac3b8219b3d95
jow1122 <ridol71534@evnft.com>
2016-09-29 19:56:00 +0100

    Optimized database queries

--- analysis.py
+++ analysis.py
@@ -139,3 +139,4 @@ Temporary commit content for 2016-07-20T19:56:00
 Temporary commit content for 2016-09-04T19:56:00
 Temporary commit content for 2016-09-05T19:56:00
 Temporary commit content for 2016-09-06T19:56:00
+Temporary commit content for 2016-09-29T19:56:00
