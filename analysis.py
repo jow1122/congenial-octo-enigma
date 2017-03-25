99cca9676221ba79d7facf9081b638016974d6aa
jow1122 <ridol71534@evnft.com>
2017-03-25 19:56:00 +0100

    Enhanced UI responsiveness

--- analysis.py
+++ analysis.py
@@ -155,3 +155,4 @@ Temporary commit content for 2017-01-29T19:56:00
 Temporary commit content for 2017-02-25T19:56:00
 Temporary commit content for 2017-03-02T19:56:00
 Temporary commit content for 2017-03-04T19:56:00
+Temporary commit content for 2017-03-25T19:56:00
