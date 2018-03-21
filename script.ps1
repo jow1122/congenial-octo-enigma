78e847375d5800a6dba8fdb7ba61efd44e602932
jow1122 <ridol71534@evnft.com>
2018-03-21 19:56:00 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -169,3 +169,4 @@ Temporary commit content for 2018-02-19T19:56:00
 Temporary commit content for 2018-02-23T19:56:00
 Temporary commit content for 2018-02-28T19:56:00
 Temporary commit content for 2018-03-19T19:56:00
+Temporary commit content for 2018-03-21T19:56:00
