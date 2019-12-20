77dfc067eaf22ca0740063aa2c4cd8386c6e1bd7
jow1122 <ridol71534@evnft.com>
2019-12-20 19:56:00 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -218,3 +218,4 @@ Temporary commit content for 2019-10-15T19:56:00
 Temporary commit content for 2019-11-01T19:56:00
 Temporary commit content for 2019-11-09T19:56:00
 Temporary commit content for 2019-11-15T19:56:00
+Temporary commit content for 2019-12-20T19:56:00
