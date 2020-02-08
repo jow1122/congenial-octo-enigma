736c3ad1670f97cec3a7715adcd076a2ebe148de
jow1122 <ridol71534@evnft.com>
2020-02-08 19:56:00 +0100

    Optimized database queries

--- script.ps1
+++ script.ps1
@@ -226,3 +226,4 @@ Temporary commit content for 2020-01-17T19:56:00
 Temporary commit content for 2020-01-23T19:56:00
 Temporary commit content for 2020-02-04T19:56:00
 Temporary commit content for 2020-02-06T19:56:00
+Temporary commit content for 2020-02-08T19:56:00
