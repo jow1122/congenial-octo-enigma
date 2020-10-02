cb6861f23c1639dacb1bf97ff45d446df344f915
jow1122 <ridol71534@evnft.com>
2020-10-02 19:56:00 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -243,3 +243,4 @@ Temporary commit content for 2020-07-13T19:56:00
 Temporary commit content for 2020-07-14T19:56:00
 Temporary commit content for 2020-09-10T19:56:00
 Temporary commit content for 2020-09-18T19:56:00
+Temporary commit content for 2020-10-02T19:56:00
