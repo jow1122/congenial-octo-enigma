a60be7a991e8f390444a1405ecf2773814c25c76
jow1122 <ridol71534@evnft.com>
2021-04-11 19:56:00 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -249,3 +249,4 @@ Temporary commit content for 2020-11-01T19:56:00
 Temporary commit content for 2020-12-17T19:56:00
 Temporary commit content for 2021-01-29T19:56:00
 Temporary commit content for 2021-04-08T19:56:00
+Temporary commit content for 2021-04-11T19:56:00
