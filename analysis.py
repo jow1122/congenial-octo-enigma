ebfc3ce63debf202a3e5ff75b594268325a0eac2
jow1122 <ridol71534@evnft.com>
2021-06-30 19:56:00 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -292,3 +292,4 @@ Temporary commit content for 2021-04-09T19:56:00
 Temporary commit content for 2021-05-15T19:56:00
 Temporary commit content for 2021-05-28T19:56:00
 Temporary commit content for 2021-06-23T19:56:00
+Temporary commit content for 2021-06-30T19:56:00
