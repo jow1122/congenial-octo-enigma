95697cf11c26e335beff1359c787ad09cb5798f4
jow1122 <ridol71534@evnft.com>
2021-07-25 19:56:00 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -295,3 +295,4 @@ Temporary commit content for 2021-06-23T19:56:00
 Temporary commit content for 2021-06-30T19:56:00
 Temporary commit content for 2021-07-19T19:56:00
 Temporary commit content for 2021-07-22T19:56:00
+Temporary commit content for 2021-07-25T19:56:00
