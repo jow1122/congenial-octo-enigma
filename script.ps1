0c14cc462dd3c53a4f3387b28636b947abe9760f
jow1122 <ridol71534@evnft.com>
2022-01-15 19:56:00 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -277,3 +277,4 @@ Temporary commit content for 2021-12-17T19:56:00
 Temporary commit content for 2021-12-30T19:56:00
 Temporary commit content for 2022-01-05T19:56:00
 Temporary commit content for 2022-01-11T19:56:00
+Temporary commit content for 2022-01-15T19:56:00
