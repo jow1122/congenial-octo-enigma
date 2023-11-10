3a3f460e6189869d1a29cbc4b86467a9964418c7
jow1122 <ridol71534@evnft.com>
2023-11-10 19:56:00 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -328,3 +328,4 @@ Temporary commit content for 2023-10-02T19:56:00
 Temporary commit content for 2023-10-09T19:56:00
 Temporary commit content for 2023-10-28T19:56:00
 Temporary commit content for 2023-10-31T19:56:00
+Temporary commit content for 2023-11-10T19:56:00
