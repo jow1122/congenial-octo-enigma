b523caff813c1a9fc5e0351ae8383d28f5942f83
jow1122 <ridol71534@evnft.com>
2023-12-19 19:56:00 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -371,3 +371,4 @@ Temporary commit content for 2023-11-15T19:56:00
 Temporary commit content for 2023-11-29T19:56:00
 Temporary commit content for 2023-12-03T19:56:00
 Temporary commit content for 2023-12-07T19:56:00
+Temporary commit content for 2023-12-19T19:56:00
