9288fbf458cc911b608720b6ff2a36000343861d
jow1122 <ridol71534@evnft.com>
2024-07-20 19:56:00 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -345,3 +345,4 @@ Temporary commit content for 2024-05-09T19:56:00
 Temporary commit content for 2024-05-27T19:56:00
 Temporary commit content for 2024-07-01T19:56:00
 Temporary commit content for 2024-07-19T19:56:00
+Temporary commit content for 2024-07-20T19:56:00
